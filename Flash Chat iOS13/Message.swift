43d47eae53c0f09efdb81bbc7394c4efc31379ab
dolcecyat <k14881488kk@mail.ru>
2024-10-14 14:45:22 +0300

    completed app

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Анатолий Коробских on 16.07.2024.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
